d38de129fed68c008462b8e797df81b4ea83ea1a
alissa_y <mintlol@mail.ru>
2023-08-23 13:25:44 +0300

    feat(app): Add mobile placeholder refs FRT-5687

---
+++ packages/app/src/app/mobile-placeholder.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react'
+import { Container, Logo, Wrapper } from '../../components/mobile-placeholder.tsx/styled'
+
+function MobilePlaceholder() {
+  return (
+    <Wrapper>
+      <img src="/img/bg.png" alt="" className="bg" />
+      <Container>
+        <Logo src="/img/logo.svg" alt="" />
+
+        <div>
+          <div className="picture">
+            <img src="/img/mobile-placeholder-sm.jpg" alt="" className="sm" />
+            <img src="/img/mobile-placeholder-md.jpg" alt="" className="md" />
+          </div>
+          <div>Waiting for you on the desktop version</div>
+        </div>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export default MobilePlaceholder
--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
+import React from 'react'
 import '@marginly/ui/styles/reset.css'
+import { LayoutSwitcher } from '../../components/mobile-placeholder.tsx/styled'
+import MobilePlaceholder from './mobile-placeholder'
 
 export default function Home() {
   return (
     <main>
-      <h2>Slender App</h2>
-      <p>TBD</p>
+      <LayoutSwitcher>
+        <div className="desktop">app </div>
+        <div className="mobile">
+          <MobilePlaceholder />
+        </div>
+      </LayoutSwitcher>{' '}
     </main>
   )
 }
