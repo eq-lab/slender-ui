6105d90635349b4a36c1549ee8b8c305a4ec76b6
Alex Demin <bouvens@gmail.com>
2023-08-30 17:21:17 +0500

    refactor(global,app): Add contract bindings

---
+++ contract-bindings/stoken/dist/types/constants.d.ts
@@ -0,0 +1,18 @@
+/**
+ * The Soroban contract ID for the stoken contract.
+ */
+export declare const CONTRACT_ID = "CC3VH7FCLXCFFPSTV2BSHFRG7ZVRZOBG5IA7LLCH3WKPIJPWFXAEV3GY";
+/**
+ * The Soroban contract ID for the stoken contract, in hex.
+ * If {@link CONTRACT_ID} is a new-style `C…` string, you will need this hex
+ * version when making calls to RPC for now.
+ */
+export declare const CONTRACT_ID_HEX: string;
+/**
+ * The Soroban network passphrase used to initialize this library.
+ */
+export declare const NETWORK_PASSPHRASE = "Test SDF Future Network ; October 2022";
+/**
+ * The Soroban RPC endpoint used to initialize this library.
+ */
+export declare const RPC_URL = "https://rpc-futurenet.stellar.org:443/soroban/rpc";
