6ac9e90f9f781baf561054e82842068c4df71235
Alex Demin <bouvens@gmail.com>
2023-08-31 12:02:12 +0500

    refactor(app): Extract discount precision const

--- packages/app/src/widgets/market-section/components/market-card.tsx
+++ packages/app/src/widgets/market-section/components/market-card.tsx
@@ -2,10 +2,10 @@ import { useDiscount } from '@/entities/market/hooks/use-discount'
 import { Token } from '@/shared/stellar-constants/tokens'
 
 export function MarketCard({ token }: { token: Token }) {
-  const discount = useDiscount(token.address)
+  const { discount, multiplier } = useDiscount(token.address)
 
   if (discount === undefined) {
     return 'Loading...'
   }
-  return <div>Discount: {Math.round(discount / 100)}%</div>
+  return <div>Discount: {(discount * 100) / multiplier}%</div>
 }
