cc5c63180728329162b3fd77928bc9f26419270b
Alex Demin <bouvens@gmail.com>
2023-09-01 19:49:07 +0500

    refactor(app): Improve useMakeGetTx

--- packages/app/src/entities/wallet/hooks/use-get-balance/use-get-balance.ts
+++ packages/app/src/entities/wallet/hooks/use-get-balance/use-get-balance.ts
@@ -1,37 +1,24 @@
 import * as SorobanClient from 'soroban-client'
 import { useEffect, useState } from 'react'
 import { useContextSelector } from 'use-context-selector'
-import { FUTURENET_NETWORK_DETAILS } from '@/shared/stellar/constants/networks'
-import { useMakeGetTx } from '@/shared/stellar/hooks/invoke'
+import { invoke, useMakeGetTx } from '@/shared/stellar/hooks/invoke'
+import { decodeStr, decodeU32, decodei128 } from '@/shared/stellar/decoders'
 import { WalletContext } from '../../context/context'
-import { accountIdentifier, decodeStr, decodeU32, decodei128 } from './decoders'
 
 interface SorobanTokenRecord {
   [key: string]: unknown
-  balance: number
+  balance: string
   name: string
   symbol: string
-  decimals: string
+  decimals: number
 }
 
-type TxToOp = {
-  [index: string]: {
-    tx: SorobanClient.Transaction<
-      SorobanClient.Memo<SorobanClient.MemoType>,
-      SorobanClient.Operation[]
-    >
-    decoder: (xdr: string) => string | number
-  }
-}
-
-const server = new SorobanClient.Server(FUTURENET_NETWORK_DETAILS.rpcUrl, {
-  allowHttp: FUTURENET_NETWORK_DETAILS.networkUrl.startsWith('http://'),
-})
+const accountIdentifier = (account: string) => new SorobanClient.Address(account).toScVal()
 
 export const useGetBalance = (tokenAddress: string) => {
-  const [balanceInfo, setBalanceInfo] = useState<SorobanTokenRecord | null>(null)
+  const [balanceInfo, setBalanceInfo] = useState<SorobanTokenRecord | null>()
   const userAddress = useContextSelector(WalletContext, (state) => state.address)
-  const getTx = useMakeGetTx(tokenAddress)
+  const makeGetTx = useMakeGetTx()
 
   useEffect(() => {
     if (!userAddress) {
@@ -39,55 +26,22 @@ export const useGetBalance = (tokenAddress: string) => {
       return
     }
 
+    const getTx = makeGetTx(tokenAddress)
     const balanceTxParams = [accountIdentifier(userAddress)]
     const balanceTx = getTx('balance', balanceTxParams)
     const nameTx = getTx('name')
     const symbolTx = getTx('symbol')
     const decimalsTx = getTx('decimals')
 
-    const txs: TxToOp = {
-      balance: {
-        tx: balanceTx,
-        decoder: decodei128,
-      },
-      name: {
-        tx: nameTx,
-        decoder: decodeStr,
-      },
-      symbol: {
-        tx: symbolTx,
-        decoder: decodeStr,
-      },
-      decimals: {
-        tx: decimalsTx,
-        decoder: decodeU32,
-      },
-    }
-
-    const tokenBalanceInfo = Object.keys(txs).reduce(
-      async (prev, curr) => {
-        const prevResult = await prev
-        const current = txs[curr]
-        if (!current) {
-          return prevResult
-        }
-        const { tx, decoder } = current
-        const { results } = await server.simulateTransaction(tx)
-        if (!results) {
-          return prevResult
-        }
-        const [result] = results
-        if (!result) {
-          return prevResult
-        }
-        prevResult[curr] = decoder(result.xdr)
-        return prevResult
-      },
-      Promise.resolve({} as SorobanTokenRecord),
-    )
-
-    tokenBalanceInfo.then((balance) => setBalanceInfo(balance))
-  }, [getTx, tokenAddress, userAddress])
+    Promise.all([
+      invoke(balanceTx, decodei128),
+      invoke(nameTx, decodeStr),
+      invoke(symbolTx, decodeStr),
+      invoke(decimalsTx, decodeU32),
+    ]).then(([balance, name, symbol, decimals]) => {
+      setBalanceInfo({ balance, name, symbol, decimals })
+    })
+  }, [makeGetTx, tokenAddress, userAddress])
 
   return balanceInfo
 }
--- packages/app/src/shared/stellar/decoders.ts
+++ packages/app/src/shared/stellar/decoders.ts
@@ -1,6 +1,6 @@
 import * as SorobanClient from 'soroban-client'
 
-const valueToI128String = (value: SorobanClient.xdr.ScVal) =>
+const valueToI128String = (value: SorobanClient.xdr.ScVal): string =>
   SorobanClient.scValToBigInt(value).toString()
 
 export const decodei128 = (b64: string) => {
@@ -8,13 +8,11 @@ export const decodei128 = (b64: string) => {
   try {
     return valueToI128String(value)
   } catch (error) {
-    return 0
+    return ''
   }
 }
 
-export const decodeStr = (b64: string) =>
+export const decodeStr = (b64: string): string =>
   SorobanClient.xdr.ScVal.fromXDR(b64, 'base64').str().toString()
 
 export const decodeU32 = (b64: string) => SorobanClient.xdr.ScVal.fromXDR(b64, 'base64').u32()
-
-export const accountIdentifier = (account: string) => new SorobanClient.Address(account).toScVal()
