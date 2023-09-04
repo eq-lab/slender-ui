1c455d9b81dfa8d0c5cca01031a5df20f7698b2d
N.Artem <N.Artem.Work@yandex.ru>
2023-09-04 13:05:17 +0300

    feat(app): Edit structure of debt position

--- packages/app/src/widgets/position-section/position-section.tsx
+++ packages/app/src/widgets/position-section/position-section.tsx
@@ -10,12 +10,10 @@ import { BorrowIncreaseModal } from '@/features/borrow-flow/components/borrow-in
 
 const getCollateralUsd = (collateral: PositionType['collaterals']) => {
   const sum = collateral.reduce((acc, elem) => {
-    if (!elem) {
-      return acc
-    }
+    if (!elem) return acc
     const { type, value } = elem
     const coinInfo = mockTokenInfoByType[type]
-    return acc + value * coinInfo.usd
+    return acc + value * coinInfo.usd * coinInfo.discount
   }, 0)
   return sum
 }
@@ -23,17 +21,62 @@ const getCollateralUsd = (collateral: PositionType['collaterals']) => {
 export function PositionSection() {
   const position = useContextSelector(PositionContext, (state) => state.position)
   const setPosition = useContextSelector(PositionContext, (state) => state.setPosition)
-  const [isDecreaseModalOpen, setIsDecreaseModalOpen] = useState(false)
-  const [isIncreaseModalOpen, setIsIncreaseModalOpen] = useState(false)
+  const [decreaseModalIndex, setDecreaseModalIndex] = useState<0 | 1 | null>(null)
+  const [increaseModalIndex, setIncreaseModalIndex] = useState<0 | 1 | null>(null)
 
   const handleDecreaseSend = (value: PositionType) => {
     setPosition(value)
-    setIsDecreaseModalOpen(false)
+    setDecreaseModalIndex(null)
   }
 
   const handleIncreaseSend = (value: PositionType) => {
     setPosition(value)
-    setIsIncreaseModalOpen(false)
+    setIncreaseModalIndex(null)
+  }
+
+  const getModalData = (modalIndex: 0 | 1 | null) => {
+    if (!position || modalIndex === null) return null
+    const debt = position.debts[modalIndex]
+    if (!debt) return null
+    return { debt, availablePosition: position }
+  }
+
+  const renderIncreaseModal = () => {
+    const modalData = getModalData(increaseModalIndex)
+    if (!modalData) return null
+    return (
+      <BorrowIncreaseModal
+        debt={modalData.debt.value}
+        collateralUsd={getCollateralUsd(modalData.availablePosition.collaterals)}
+        type={modalData.debt.type}
+        onClose={() => setIncreaseModalIndex(null)}
+        onSend={(value) =>
+          handleIncreaseSend({
+            debts: value,
+            collaterals: modalData.availablePosition.collaterals,
+          })
+        }
+      />
+    )
+  }
+
+  const renderDecreaseModal = () => {
+    const modalData = getModalData(decreaseModalIndex)
+    if (!modalData) return null
+    return (
+      <BorrowDecreaseModal
+        debt={modalData.debt.value}
+        collateralUsd={getCollateralUsd(modalData.availablePosition.collaterals)}
+        type={modalData.debt.type}
+        onClose={() => setDecreaseModalIndex(null)}
+        onSend={(value) =>
+          handleDecreaseSend({
+            debts: value,
+            collaterals: modalData.availablePosition.collaterals,
+          })
+        }
+      />
+    )
   }
 
   return (
@@ -59,47 +102,31 @@ export function PositionSection() {
           <div>
             <h4>borrowed</h4>
             <div>
-              {position.debt} {position.debtType}{' '}
-              <button type="button" onClick={() => setIsDecreaseModalOpen(true)}>
-                -
-              </button>
-              <button type="button" onClick={() => setIsIncreaseModalOpen(true)}>
-                +
-              </button>
+              {position.debts.map((debt, index) => {
+                if (!debt) return null
+                if (index === 0 || index === 1) {
+                  return (
+                    <>
+                      {debt.value} {debt.type}{' '}
+                      <button type="button" onClick={() => setDecreaseModalIndex(index)}>
+                        -
+                      </button>
+                      <button type="button" onClick={() => setIncreaseModalIndex(index)}>
+                        +
+                      </button>
+                    </>
+                  )
+                }
+                return null
+              })}
             </div>
           </div>
         </div>
       ) : (
         <>empty</>
       )}
-      {isDecreaseModalOpen && position && (
-        <BorrowDecreaseModal
-          debt={position.debt}
-          collateralUsd={getCollateralUsd(position.collaterals)}
-          type={position.debtType}
-          onClose={() => setIsDecreaseModalOpen(false)}
-          onSend={(value) =>
-            handleDecreaseSend({
-              ...value,
-              collaterals: position.collaterals,
-            })
-          }
-        />
-      )}
-      {isIncreaseModalOpen && position && (
-        <BorrowIncreaseModal
-          debt={position.debt}
-          collateralUsd={getCollateralUsd(position.collaterals)}
-          type={position.debtType}
-          onClose={() => setIsIncreaseModalOpen(false)}
-          onSend={(value) =>
-            handleIncreaseSend({
-              ...value,
-              collaterals: position.collaterals,
-            })
-          }
-        />
-      )}
+      {renderDecreaseModal()}
+      {renderIncreaseModal()}
     </div>
   )
 }
