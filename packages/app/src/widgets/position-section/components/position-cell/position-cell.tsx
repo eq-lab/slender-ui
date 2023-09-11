f8cd712d6313018f0410ef2128377ec55e814152
Alex Demin <bouvens@gmail.com>
2023-09-11 21:30:35 +0500

    refactor(app): Remove unneeded comments

--- packages/app/src/widgets/position-section/components/position-cell/position-cell.tsx
+++ packages/app/src/widgets/position-section/components/position-cell/position-cell.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react'
 import { tokenContracts } from '@/shared/stellar/constants/tokens'
 import { PositionCell as PositionCellType } from '@/entities/position/types'
