59f63aadb178d9c7f99f8eb7b04318829183a642
Alex Demin <bouvens@gmail.com>
2023-09-15 19:18:13 +0500

    style(app): Align zero values

--- packages/app/src/features/liquidity-flow/components/asset-select/styled.ts
+++ packages/app/src/features/liquidity-flow/components/asset-select/styled.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
-import { ThumbnailWrapper as ThumbnailWrapperUi } from '@marginly/ui/components/thumbnail/thumbnail.styled'
+import { ThumbnailWrapper as ThumbnailWrapperUi } from '@marginly/ui/components/thumbnail/styled'
 
 export const ThumbnailWrapper = styled(ThumbnailWrapperUi)`
   box-shadow:
-    0px 4px 8px 0px rgba(51, 20, 0, 0.08),
-    0px 2px 1px 0px rgba(51, 20, 0, 0.04);
+    0 4px 8px 0 rgba(51, 20, 0, 0.08),
+    0 2px 1px 0 rgba(51, 20, 0, 0.04);
   cursor: pointer;
 `
 
