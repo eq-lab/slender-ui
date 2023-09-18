5040ddad164b831b29bb4f7423b90adb47c29ef8
Alex Demin <bouvens@gmail.com>
2023-09-18 19:14:01 +0500

    refactor(app): Update image imports

--- packages/app/src/global/mobile-placeholder/mobile-placeholder.tsx
+++ packages/app/src/global/mobile-placeholder/mobile-placeholder.tsx
@@ -1,19 +1,47 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image'
 import { Container, Logo, Wrapper } from './styled'
+import backgroundImage from './images/bg.png'
+import smPlaceholderImage from './images/mobile-placeholder-sm.jpg'
+import mdPlaceholderImage from './images/mobile-placeholder-md.jpg'
 
 export function MobilePlaceholder() {
   return (
     <Wrapper>
-      <img src="/img/bg.png" alt="" className="bg" />
+      <Image
+        src={backgroundImage}
+        alt=""
+        className="bg"
+        style={{
+          objectFit: 'contain',
+        }}
+        sizes="100vw"
+      />
       <Container>
         <Logo />
 
         <div>
           <div className="picture">
-            <img src="/img/mobile-placeholder-sm.jpg" alt="" className="sm" />
-            <img src="/img/mobile-placeholder-md.jpg" alt="" className="md" />
+            <Image
+              src={smPlaceholderImage}
+              alt=""
+              className="sm"
+              style={{
+                objectFit: 'contain',
+              }}
+              sizes="100vw"
+            />
+            <Image
+              src={mdPlaceholderImage}
+              alt=""
+              className="md"
+              style={{
+                objectFit: 'contain',
+              }}
+              sizes="100vw"
+            />
           </div>
           <div>Waiting for you on the desktop version</div>
         </div>
