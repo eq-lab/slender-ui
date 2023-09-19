b13a843c63359908e1aec655db81d3e405ecd1d8
Alex Demin <bouvens@gmail.com>
2023-09-19 13:15:58 +0500

    fix(app): Multiply positions to decimals for bindings

--- packages/app/src/features/liquidity-flow/hooks/use-liquidity.ts
+++ packages/app/src/features/liquidity-flow/hooks/use-liquidity.ts
@@ -3,9 +3,19 @@ import { PositionContext } from '@/entities/position/context/context'
 import { PositionCell } from '@/entities/position/types'
 import { useWalletAddress } from '@/shared/contexts/use-wallet-address'
 import { logError, logInfo } from '@/shared/logger'
+import { useTokenCache } from '@/entities/token/context/hooks'
+import { CachedTokens } from '@/entities/token/context/context'
 import { useSetWaitModalIsOpen } from '../context/hooks'
 import { PositionUpdate } from '../types'
-import { getPositionUpdateByCells } from '../soroban/get-position-update-by-cells'
+
+export const getPositionUpdateByCells = (
+  positionCells: PositionCell[],
+  tokenCache: Partial<CachedTokens> = {},
+): PositionUpdate =>
+  positionCells.reduce((positionUpdate: PositionUpdate, item: PositionCell): PositionUpdate => {
+    positionUpdate[item.tokenName] = item.value * BigInt(tokenCache[item.tokenName]?.decimals ?? 1)
+    return positionUpdate
+  }, {} as PositionUpdate)
 
 export function useLiquidity(): (args: {
   submitFunc: (address: string, sendValue: PositionUpdate) => Promise<'fulfilled' | never>
@@ -19,6 +29,7 @@ export function useLiquidity(): (args: {
   const setWaitModalIsOpen = useSetWaitModalIsOpen()
   const { address } = useWalletAddress()
   const position = useContextSelector(PositionContext, (state) => state.position)
+  const tokenInfo = useTokenCache()
 
   return async (args) => {
     if (!address) return false
@@ -28,6 +39,7 @@ export function useLiquidity(): (args: {
 
       const positionUpdate = getPositionUpdateByCells(
         args.additionalDeposits ?? args.additionalDebts ?? args.deposits ?? args.debts ?? [],
+        tokenInfo,
       )
       const result = await args.submitFunc(address, positionUpdate)
       logInfo(result)
