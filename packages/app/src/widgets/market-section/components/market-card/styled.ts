9728105787ee7c5dbd454654f5599ce05742de1a
Vilmir Baikov <noname777228@rambler.ru>
2023-09-20 16:37:05 +0300

    fix(app): Market card paddings

--- packages/app/src/widgets/market-section/components/market-card/styled.ts
+++ packages/app/src/widgets/market-section/components/market-card/styled.ts
@@ -41,7 +41,7 @@ export const MarketCardPoolInfoContainer = styled.div`
   grid-template-columns: 48px 1fr;
   grid-template-rows: repeat(2, 1fr);
   grid-column-gap: 16px;
-  padding: 64px 0 24px;
+  padding-top: 64px;
 
   .piechart-with-tooltip-wrapper {
     position: relative;
