e3723e8a2aa6ea7096356ebf47a7c86cea92a627
Artem N <N.Artem.Work@yandex.ru>
2023-09-29 16:00:43 +0400

    feat(app): Updated TokenSuperField

--- packages/app/src/shared/components/token-super-field/token-super-field.tsx
+++ packages/app/src/shared/components/token-super-field/token-super-field.tsx
@@ -1,6 +1,9 @@
 import React, { useCallback } from 'react'
 import { SuperField } from '@marginly/ui/components/input/super-field'
 import { useFocus } from '@marginly/ui/components/input/use-focus'
+import { FieldContainer } from '@marginly/ui/components/input/text.styled'
+import { Suggestion } from '@marginly/ui/constants/classnames'
+import Label from '@marginly/ui/components/label'
 
 const NUMBER_INPUT_REGEX = /^[0-9]+(?:\.[0-9]*)?$/
 
@@ -8,28 +11,29 @@ interface Props {
   onChange: (value: string) => void
   value: string
   title: string
-  placeholder: string
-  postfix?: string
   className?: string
   initFocus?: boolean
+  tokenSymbol?: string
+  badgeValue?: string
 }
 
 export function TokenSuperField({
   onChange,
-  placeholder,
-  postfix,
   title,
   value,
   className,
   initFocus,
+  tokenSymbol,
+  badgeValue,
 }: Props) {
   const { focused, onClick, ref, setInputFocused } = useFocus()
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const changedValue = event.target.value
-    const isCorrectValue = (changedValue || '0').match(NUMBER_INPUT_REGEX)
+    const changedModifiedValue = changedValue.startsWith('.') ? `0${changedValue}` : changedValue
+    const isCorrectValue = (changedModifiedValue || '0').match(NUMBER_INPUT_REGEX)
     if (!isCorrectValue) return
-    onChange(changedValue)
+    onChange(changedModifiedValue)
   }
 
   const refCallback = useCallback(
@@ -46,14 +50,22 @@ export function TokenSuperField({
   return (
     <SuperField
       onChange={handleChange}
-      placeholder={placeholder}
-      postfix={postfix}
+      placeholder={`${tokenSymbol} amount`}
+      postfix={tokenSymbol}
       title={title}
       value={value}
       className={className}
       onClick={onClick}
       ref={refCallback}
       focused={focused}
-    />
+    >
+      {Number(badgeValue) && badgeValue ? (
+        <FieldContainer className={Suggestion}>
+          <Label badge onClick={() => onChange(badgeValue)}>
+            Max {badgeValue}
+          </Label>
+        </FieldContainer>
+      ) : null}
+    </SuperField>
   )
 }
