816de86b9364ca83a27b8b9821bf0c4949136c1f
Vilmir Baikov <noname777228@rambler.ru>
2023-09-29 16:08:15 +0300

    refactor(app): Tolltip with classname consumer

--- packages/app/src/shared/components/tooltip/tooltip.tsx
+++ packages/app/src/shared/components/tooltip/tooltip.tsx
@@ -1,17 +1,15 @@
 import React, { ReactElement } from 'react'
 import TooltipUi from '@mui/material/Tooltip'
+import * as S from './styled'
 
 export function Tooltip({ children, content }: { children: ReactElement; content: ReactElement }) {
   return (
-    <TooltipUi
-      title={content}
-      arrow
-      componentsProps={{
-        tooltip: { sx: { padding: '12px', borderRadius: '12px', backgroundColor: '#000' } },
-        arrow: { sx: { color: '#000' } },
-      }}
-    >
-      {children}
-    </TooltipUi>
+    <S.GeneratePopperClassName>
+      {(popperClassName) => (
+        <TooltipUi title={content} arrow classes={{ popper: popperClassName }}>
+          {children}
+        </TooltipUi>
+      )}
+    </S.GeneratePopperClassName>
   )
 }
