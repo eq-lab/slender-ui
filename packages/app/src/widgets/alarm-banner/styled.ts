5ccc3f1cfcdf3c9961e907b3e82677fbe2f3628d
Artem N <N.Artem.Work@yandex.ru>
2024-03-12 16:03:06 +0800

    feat(app): Add banner

---
+++ packages/app/src/widgets/alarm-banner/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import Banner from '@marginly/ui/components/banner';
+
+export const Wrapper = styled(Banner)`
+  margin-top: 64px;
+  svg path {
+    fill: var(--icon-negative);
+  }
+`;
