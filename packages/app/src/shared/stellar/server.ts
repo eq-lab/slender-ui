ca2c2dfc41deb6004fe16b1c3b86ddcba2648064
Konstantin Latypov <konstantin4job@gmail.com>
2025-09-24 10:29:19 +0200

    fix: Upgrade node and stellar-sdk versions

--- packages/app/src/shared/stellar/server.ts
+++ packages/app/src/shared/stellar/server.ts
@@ -1,8 +1,8 @@
-import { SorobanRpc } from '@stellar/stellar-sdk';
+import { Horizon } from '@stellar/stellar-sdk';
 import { NETWORK_DETAILS } from './constants/networks';
 
 const SERVER_URL = NETWORK_DETAILS.rpcUrl;
 
-export const server = new SorobanRpc.Server(SERVER_URL, {
+export const server = new Horizon.Server(SERVER_URL, {
   allowHttp: SERVER_URL.startsWith('http://'),
 });
