c8d908876b15339a5c16b365d1cf54ed691b3275
Konstantin Latypov <konstantin4job@gmail.com>
2025-10-02 14:59:34 +0200

    fix: Import rpc

--- packages/app/src/shared/stellar/server.ts
+++ packages/app/src/shared/stellar/server.ts
@@ -1,8 +1,8 @@
-import { Server } from '@stellar/stellar-sdk/rpc';
+import { rpc } from '@stellar/stellar-sdk';
 import { NETWORK_DETAILS } from './constants/networks';
 
 const SERVER_URL = NETWORK_DETAILS.rpcUrl;
 
-export const server = new Server(SERVER_URL, {
+export const server = new rpc.Server(SERVER_URL, {
   allowHttp: SERVER_URL.startsWith('http://'),
 });
